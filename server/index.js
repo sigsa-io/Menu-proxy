172906509e87e9ba5c375d17472fdd42dcbf1eda
Minho Choo <mchoo7@gatech.edu>
2019-07-15 20:28:02 -0700

    setup the proxy server

---
+++ server/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const httpProxy = require('http-proxy');
+const apiProxy = httpProxy.createProxyServer();
+const serverOne = 'http://localhost:3000';
+const serverTwo = 'http://localhost:3002';
+const serverThree = 'http://localhost:3004';
+
+app.use('/:restaurantId', express.static(path.resolve(__dirname, '..', 'client', 'dist')));
+
+app.all("/restaurantName/:restaurantId", function(req, res) {
+  console.log('redirecting to Server1');
+  apiProxy.web(req, res, {target: serverOne});
+});
+
+app.all("/bookingCount/:restaurantId", function(req, res) {
+  console.log('redirecting to Server1');
+  apiProxy.web(req, res, {target: serverOne});
+});
+
+app.all("/seatingSize/:restaurantId", function(req, res) {
+  console.log('redirecting to Server1');
+  apiProxy.web(req, res, {target: serverOne});
+});
+
+app.all("/targettimeslots/:restaurantId", function(req, res) {
+  console.log('redirecting to Server1');
+  apiProxy.web(req, res, {target: serverOne});
+});
+
+app.all("/reservations/:restaurantId", function(req, res) {
+  console.log('redirecting to Server1');
+  apiProxy.web(req, res, {target: serverOne});
+});
+
+app.all("/menu/:restaurantId", function(req, res) {
+  console.log('redirecting to Server2');
+  apiProxy.web(req, res, {target: serverTwo});
+});
+
+app.all("/restaurants/:restaurantId/reviews", function(req, res) {
+  console.log('redirecting to Server3');
+  apiProxy.web(req, res, {target: serverThree});
+});
+
+app.listen(3001);
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.js`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
+  resolve: { extensions: ['.js', '.jsx'] },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+      {
+        test: /\.css/,
+        include: SRC_DIR,
+        use: [{
+          loader: 'style-loader',
+        },
+        {
+          loader: 'css-loader',
+          options: {
+            modules: {
+              localIdentName: '[local]__[hash:base64:5]',
+            },
+          },
+        }],
+      },
+    ],
+  },
+};
